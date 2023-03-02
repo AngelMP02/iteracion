eac6b85b4bf27fc61bfc0f06712c2f49b59b0f91
Agirre8 <inagi30@gmail.com>
2023-03-02 16:42:08 +0100

    Update ej12.py

--- Ejercicios/ej12.py
+++ Ejercicios/ej12.py
@@ -1,25 +1,35 @@
 from math import sqrt
 
 numeros = []
-n=1
+
 rango = int(input("Introduzca el rango de numeros de los que quiere sacar los cuadrados perfectos:"))
 cuadrados_perfectos = []
 
 #bucle para añadir los numeros a la lista 
-for i in range(0,rango):
-    numeros.append(n)
-    n +=1
-print(numeros)
+def funcionCuadradosPerfectos(rango):
+    n=1
+    for i in range(0,rango):
+        numeros.append(n)
+        n +=1
+    print(numeros)
 
-while rango !=0 :
-    rango -= 1
+    while rango !=0 :
+        rango -= 1
 
-    raiz = sqrt(numeros[rango])
+        raiz = sqrt(numeros[rango])
 
-    if int(raiz) == raiz:
-        raiz = raiz * raiz #la operacion al cuadrado no me deja hacerla por un error entre float e int
-        cuadrados_perfectos.append(raiz)
+        if int(raiz) == raiz:
+            raiz = raiz * raiz #la operacion al cuadrado no me deja hacerla por un error entre float e int
+            cuadrados_perfectos.append(raiz)
 
+funcionCuadradosPerfectos(rango)
 print(f"Los cuadrados perfectos en el rango de numeros {rango} son {cuadrados_perfectos}")
 
-   
+
+def sqrt(n):
+  if n < 0:
+    return
+  else:
+    return n**0.5
+  
+print(sqrt(16))
