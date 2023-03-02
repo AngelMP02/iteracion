30b4d5fd51a2dcb01c19b3ebe9e98ec4c8632e0e
Angel <angelmp200@gmail.com>
2023-03-02 16:32:16 +0100

    Update ej10.py

--- ej10.py
+++ ej10.py
@@ -4,5 +4,12 @@ class Persona:
         self.edad = edad
         self.padre = padre
         self.madre = madre
+familias = [None] * 1000 #creo una lista con 1000 elementos
+
+personas_entre_20_30 = []
+def personas_20_30():
+        for persona in familias:
+            if persona is not None and 20 <= persona.edad <= 30:
+                personas_20_30.append(persona)
 
 
