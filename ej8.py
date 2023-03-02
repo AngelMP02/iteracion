edfcf535e953531f7fa7c74389c7e6c059384b07
Angel <angelmp200@gmail.com>
2023-03-02 15:43:01 +0100

    Update ej8.py

--- ej8.py
+++ ej8.py
@@ -1,9 +1,12 @@
+import tabulate
 def separador(cadena):  
     cadena.split(":")
     lista=[]
     for i in range(0,len(cadena)):
         lista.append(list(cadena[i:i+1]))
         tabla=lista
+        encabezado= ["Numero", "Cadena"]
+        print(tabulate.tabulate(tabla, encabezado, showindex = True))
 
 
     
