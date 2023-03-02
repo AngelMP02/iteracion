a48b53ae3fb3e789852fee34fb5ec4c909faf813
Angel <angelmp200@gmail.com>
2023-03-02 16:25:30 +0100

    Update ej10.py

---
+++ ej10.py
@@ -0,0 +1,8 @@
+class Persona:
+    def __init__(self, identidad, edad, padre, madre):
+        self.identidad = identidad
+        self.edad = edad
+        self.padre = padre
+        self.madre = madre
+
+
