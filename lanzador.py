2d0b694a53cf264691282c84e9d25a1974a9997f
Agirre8 <inagi30@gmail.com>
2023-03-02 16:56:40 +0100

    ej7 acabado

--- lanzador.py
+++ lanzador.py
@@ -22,8 +22,18 @@ def ejecutar():
     if n == 6:
         return
 
+
     elif n==7:
-        return
+        try:
+            numero=int(input("Introduzca un número:"))
+        except ValueError:
+            print("El input no es un número entero")
+        try:
+            base=int(input("Introduzca una base 2 o mayor:"))
+        except ValueError:
+            print("El input no es un número entero")
+
+        transformar(numero, base)  
 
 
     elif n==8:
