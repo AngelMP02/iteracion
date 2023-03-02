4bf6708ca61b6f1ab594827f318edc06072d4c66
Angel <angelmp200@gmail.com>
2023-03-02 16:01:03 +0100

    Update ej9.py

--- ej9.py
+++ ej9.py
@@ -1,14 +1,13 @@
 import tabulate
-diccionario=["avion", "tren", "auto", "camion"]
 def tabla(diccionario):
     table=[]
-    diccionario.sort()
     for i in range(0,len(diccionario)):
         table.append(diccionario[i:i+1])
     encabezado=["i", "diccionario", "anterior", "siguiente"]
     print(tabulate.tabulate(table, encabezado, showindex = True))
 
+tabla(diccionario=["avion", "tren", "auto", "camion"])
+
 
    
 
-tabla(diccionario)
