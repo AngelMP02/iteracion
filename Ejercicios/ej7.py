25b9732ceb0d24a242beefa8fc54340190d84bf1
Agirre8 <inagi30@gmail.com>
2023-03-02 16:56:44 +0100

    +

--- Ejercicios/ej7.py
+++ Ejercicios/ej7.py
@@ -9,6 +9,22 @@ except ValueError:
     print("El input no es un número entero")
 
 
-def transformar(base, numero):
-    return
-    
+def transformar(numero, base):
+    if base < 2:
+        return "La base tiene que ser 2 o mayor"
+    elif base >36:
+        return numero
+    elif numero <= 0:
+        print("El numero tiene que ser mayor que 0")
+    else:
+        conversion = ""
+        while numero > 0:
+            residuo = numero % base
+            if residuo < 10:
+                conversion = str(residuo) + conversion
+            else:
+                vonversion = chr(residuo - 10 + ord('A')) + conversion
+            numero = numero // base
+        print(conversion)
+
+transformar(numero, base)   
