4d41bbda07ae5187444646440966b9cb65d1362d
Angel <angelmp200@gmail.com>
2023-03-02 16:44:01 +0100

    Update ej10.py

---
+++ Ejercicios/ej10.py
@@ -0,0 +1,26 @@
+class PERSONA:
+    def __init__(self, id_persona, nombre, apellido, edad, id_padre, id_madre):
+        self.id_persona = id_persona
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.id_padre = id_padre
+        self.id_madre = id_madre
+
+
+if __name__ == '__main__':
+    familias = [None] * 1000
+
+    familias = [None] * 1000 #creo una lista con 1000 elementos
+
+    personas_entre_20_30 = []
+    def personas_20_30():
+            for persona in familias:
+                if persona is not None and 20 <= persona.edad <= 30:
+                    personas_entre_20_30.append(persona)
+    
+    huérfanos_menos_15 = []
+    def numero_huerfanos_menor_15():
+        for persona in familias:
+            if persona is not None and persona.edad < 15 and persona.id_padre == "HUÉRFANO" and persona.id_madre == "HUÉRFANO":
+                huérfanos_menos_15.append(persona)
